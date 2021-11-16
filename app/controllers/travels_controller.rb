61bbcb14a87cb97c51e02b2cbbffb54afbf3a37b
Christopher Marchand <chris.marchand.pro@gmail.com>
2021-11-16 16:50:54 +0100

    front de la show en route

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,6 +1,13 @@
 class TravelsController < ApplicationController
+  before_action :set_travel, only: :show
 
   def show
+  end
+
+  private
+
+  def set_travel
     @travel = Travel.find(params[:id])
+    authorize @travel
   end
 end
