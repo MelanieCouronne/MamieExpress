f7ef8bdbe1947cbfa864dc8e7c507a6ed0ff389e
Christopher Marchand <chris.marchand.pro@gmail.com>
2021-11-16 15:26:30 +0100

    controller travels add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :grannies, :travels
-  
+  has_many :grannies
+  has_many :travels
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
