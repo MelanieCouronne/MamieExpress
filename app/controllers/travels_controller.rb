c2493a80ff40b6644ebd797646d0200179b30668
Melanie Couronne <melanie.couronne@gmail.com>
2021-11-16 17:32:08 +0100

    Add create action to travels controller

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -4,7 +4,23 @@ class TravelsController < ApplicationController
     authorize @travel
   end
 
+  def create
+    @travel = Travel.new(travel_params)
+    if travel.valid?
+      @travel.save
+      redirect_to travel_path(@travel)
+    else
+      render :new
+    end
+  end
+
   def show
     @travel = Travel.find(params[:id])
+  end
+
+  private
+
+  def travel_params
+    params.require(:travel).permit(:departure_location, :departure_date, :arrival_location, :arrival_date, :number_passenger)
   end
 end
