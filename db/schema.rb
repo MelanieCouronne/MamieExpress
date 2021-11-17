b739885ca6834292f49dcd94b0b3651072873d02
Christopher Marchand <chris.marchand.pro@gmail.com>
2021-11-17 12:52:19 +0100

    add de la map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_151705) do
+ActiveRecord::Schema.define(version: 2021_11_17_095244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_151705) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "arrival_location"
     t.string "departure_location"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_travels_on_user_id"
   end
 
